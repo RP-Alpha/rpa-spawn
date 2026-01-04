1fa9c36655a3e3d237a515594df7a3dd95b4f3f0
Loafabreadly <birdgeekthree@gmail.com>
2026-01-04 07:48:11 -0700

    feat: add permission configuration to config.lua
    - Added AdminPermissions for spawn location management
    - Updated spawn locations to use new permissions format
    - Replaced simple 'jobs' field with full permission object

--- config.lua
+++ config.lua
@@ -1,28 +1,59 @@
 Config = {}
 
+-- ============================================
+-- PERMISSION CONFIGURATION
+-- ============================================
+
+-- Admin permissions (can manage spawn locations in-game)
+Config.AdminPermissions = {
+    groups = { 'admin', 'god' },
+    jobs = {},
+    minGrade = 0,
+    onDuty = false,
+    convar = 'rpa:admins',
+    resourceConvar = 'admin'
+}
+
+-- ============================================
+-- SPAWN LOCATIONS
+-- ============================================
+
 -- Note: Replace these placeholder image URLs with actual screenshots of each location
 Config.Spawns = {
     ['mrpd'] = {
         label = "Mission Row PD",
         coords = vector4(428.23, -984.28, 29.76, 3.5),
         image = "https://i.imgur.com/iqJ2wLF.png", -- Mission Row PD
-        jobs = { 'police' } -- Optional: restrict to certain jobs
+        -- Permissions for this spawn (uses rpa-lib HasPermission)
+        permissions = {
+            jobs = { 'police' },
+            groups = {},
+            minGrade = 0,
+            onDuty = false
+        }
     },
     ['pillbox'] = {
         label = "Pillbox Hill Medical",
         coords = vector4(298.6, -584.6, 43.26, 74.5),
         image = "https://i.imgur.com/6gT5xYg.png", -- Pillbox Hospital
-        jobs = { 'ambulance', 'ems' }
+        permissions = {
+            jobs = { 'ambulance', 'ems' },
+            groups = {},
+            minGrade = 0,
+            onDuty = false
+        }
     },
     ['legion'] = {
         label = "Legion Square",
         coords = vector4(215.8, -856.3, 30.1, 340.5),
-        image = "https://i.imgur.com/bTYmKbU.png" -- Legion Square
+        image = "https://i.imgur.com/bTYmKbU.png", -- Legion Square
+        permissions = nil -- nil = available to everyone
     },
     ['airport'] = {
         label = "Los Santos Airport",
         coords = vector4(-1037.7, -2737.7, 20.1, 330.0),
-        image = "https://i.imgur.com/qkPBOLJ.png" -- Airport
+        image = "https://i.imgur.com/qkPBOLJ.png", -- Airport
+        permissions = nil
     }
 }
 
